692d5286aa1a61e3bba8afb9e598b3862be753b0
Zlaam <mrzalaam@gmail.com>
2024-07-08 21:41:23 +0500

    update ep01/index.ts

--- ep01/index.ts
+++ ep01/index.ts
@@ -1,9 +1,1 @@
-const students = ["siraj", "khan", "zlaam", "husnain"];
-function returnStudents(students: string[], studentName: string) {
-  const foundStudent = students.find((students) =>
-    students.includes(studentName)
-  );
-  return foundStudent ? foundStudent : "No result found";
-}
-
-console.log(returnStudents(students, "khan"));
+const groceries = ["milk", "bread", "eggs", "flour", "cheese", "sugar"];
