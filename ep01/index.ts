22299f5bb49dc7afe023ac1db05a9fac625251f4
Zlaam <mrzalaam@gmail.com>
2024-07-12 20:46:49 +0500

    update ep01/index.ts

--- ep01/index.ts
+++ ep01/index.ts
@@ -1,12 +1,12 @@
-// const groceries = ["milk", "bread", "eggs", "flour", "cheese", "sugar"];
-// const searchForItem = (item: string) => {
-//   for (const i = 0; i < groceries.length; i++) {
-//     if (groceries[i] === item) {
-//       console.log(`${item} is best for me`);
-//     }
-//   }
-// };
-// searchForItem("eggs");
+const groceries = ["milk", "bread", "eggs", "flour", "cheese", "sugar"];
+const searchForItem = (item: string) => {
+  for (let i = 0; i < groceries.length; i++) {
+    if (groceries[i] === item) {
+      console.log(`${item} is best for me`);
+    }
+  }
+};
+searchForItem("eggs");
 
 const hello = "hello const";
 
